73f5c4ba62e487fb1d2f925f98d41b11e65ac9ed
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-03-08 13:17:01 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+"""GitHub Classroom autograding script."""
+
+import os
+
+if not os.path.exists("shipping_per_warehouse.png"):
+    raise FileNotFoundError("File 'shipping_per_warehouse.png' not found")
+
+if not os.path.exists("mode_of_shipment.png"):
+    raise FileNotFoundError("File 'mode_of_shipment.png' not found")
+
+if not os.path.exists("average_customer_rating.png"):
+    raise FileNotFoundError("File 'average_customer_rating.png' not found")
+
+if not os.path.exists("weight_distribution.png"):
+    raise FileNotFoundError("File 'weight_distribution.png' not found")
+
+if not os.path.exists("index.html"):
+    raise FileNotFoundError("File 'index.html' not found")
+
